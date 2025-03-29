d87f935ece1d5ba010e38590cb48d043138dce95
Treevyy <aarontrevormoore@gmail.com>
2025-03-29 05:56:27 -0500

    added canadiate determination to be saved in local storage

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -16,6 +16,23 @@ const CandidateSearch = () => {
     bio: null
   })
   
+  const candidateDetermination = async (candidateIsChoosen: boolean) => {
+    if (candidateIsChoosen) {
+      let parsedCandidates: Candidate[] = [];
+      const savedCandidates = localStorage.getItem('savedCandidates')
+      if (typeof savedCandidates === 'string') {
+      parsedCandidates = JSON.parse(savedCandidates);
+    }
+    parsedCandidates.push(selectedCandidate);
+    localStorage.setItem('savedCandidates', JSON.stringify(parsedCandidates));
+  };
+  if (currentCandidateId + 1 < results.length) {
+    setCurrentCandidateId(currentCandidateId + 1);
+    await searchSpecificCandidate(results[currentCandidateId + 1].login || '')
+    setCurrentCandidateId(0);
+    await searchSpecificCandidate();
+  };
+
   const [currentCandidateId, setCurrentCandidateId] = useState<number>(0);
 
   const searchSpecificCandidate = async (user: string) => {
