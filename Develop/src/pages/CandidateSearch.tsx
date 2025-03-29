bd7f965560d374fffaff67fdbb66b892377bdf43
Treevyy <aarontrevormoore@gmail.com>
2025-03-29 03:37:38 -0500

    added inital state and default values for candidate search component

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,7 +1,20 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import type { Candidate } from '../interfaces/Candidate.interface.tsx'
 
 const CandidateSearch = () => {
+  const [Candidate, setCandidate] = useState<Candidate[]>([]);
+  const [results, setResults] = useState<Candidate>({
+    login: '',
+    id: '',
+    avatar_url: '',
+    html_url: '',
+    name: '',
+    location: '',
+    email: '',
+    company: '',
+    bio: ''
+  })
   return <h1>CandidateSearch</h1>;
 };
 
