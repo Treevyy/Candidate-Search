ddc81c6c99ebce08c45dfdcb8467e5c6e23ead60
Treevyy <aarontrevormoore@gmail.com>
2025-03-29 03:11:28 -0500

    updated interface data

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,8 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 
 export interface Candidate {
-    avatar: string;
+    login: string;
     id: number;
+    avatar_url: string;
+    html_url: string;
     name: string;
     location: string;
     email: string;
