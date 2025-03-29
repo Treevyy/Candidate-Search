1e1c67d7fdaf8da63f6f12ed314b8d8b3b894d0b
Treevyy <aarontrevormoore@gmail.com>
2025-03-29 02:49:46 -0500

    added interface data

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+
+export interface Candidate {
+    avatar: string;
+    id: number;
+    name: string;
+    location: string;
+    email: string;
+    company: string;
+    bio: string;
+}
