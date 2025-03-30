fa67aea07dc2fba2259e08f41c4a6efb7b0fe0d8
Treevyy <aarontrevormoore@gmail.com>
2025-03-29 19:43:32 -0500

    added candidate card

---
+++ Develop/src/components/CandidateCard.tsx
@@ -0,0 +1,90 @@
+import type { Candidate } from '../interfaces/Candidate.interface.tsx'
+import { IoAddCircle, IoRemoveCircle } from 'react-icons/io5'
+
+type CandidateCardProps = {
+    currentCandidate: Candidate;
+    candidateDetermination: (candidateIsChoosen: boolean) => void;
+};
+
+const CandidateCard = ({ currentCandidate, candidateDetermination }: CandidateCardProps) => {
+    return (
+        <section>
+            {currentCandidate?.login ? (
+                <>
+                    {currentCandidate?.avatar_url ? (
+                        <img
+                            src={currentCandidate.avatar_url}
+                            alt={`Avatar of ${currentCandidate.login}`}
+                            style={{ width: '100px', height: '100px' }}
+                        />
+                    ) : (
+                        <img
+                            src="https://via.placeholder.com/100"
+                            alt={`Avatar of ${currentCandidate.login}`}
+                            style={{ width: '100px', height: '100px' }}
+                        />
+                    )}
+
+                    <section
+                        style={{
+                            backgroundColor: '#000',
+                            width: '280px',
+                        }}
+                    >
+                        {currentCandidate?.html_url && currentCandidate?.login ? (
+                            <a href={currentCandidate.html_url} target="_blank" rel="noreferrer">
+                                <h2 
+                                    style={{ padding: 0, margin: '-7px 0 0 0', color: 'white'}}
+                                >
+                                    {currentCandidate.name}
+                                    <em>({currentCandidate.login})</em>
+                                </h2>
+                            </a>
+                        ) : null}
+                        {currentCandidate?.location ? (
+                            <p>Location: {currentCandidate.location}</p>
+                        ) : null}
+                        {currentCandidate?.email ? (
+                            <p>
+                                Email: {''}
+                                <a href={`mailto:${currentCandidate.email}`}> {currentCandidate.email}</a>
+                            </p>
+                        ) : null}
+                        {currentCandidate?.company ? (
+                            <p>Company: {currentCandidate.company}</p>
+                        ) : null}
+                        {currentCandidate?.bio ? <p>Bio: {currentCandidate.bio}</p>
+                        : null}
+                    </section>
+                    <section
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            marginTop: '10px',
+                        }}
+                    >
+                        <IoRemoveCircle
+                            style={{
+                                color: 'red',
+                                fontSize: '24px',
+                                cursor: 'pointer',
+                            }}
+                            onClick={ () => {candidateDetermination(false) }}
+                            />
+
+                        <IoAddCircle
+                            onClick={() => {candidateDetermination(true)}}
+                            style={{
+                                color: 'green',
+                                fontSize: '24px',
+                                cursor: 'pointer',
+                            }}
+                        />
+                    </section>
+                </>
+            ) : (
+                <h2>No Candidates at this time</h2>    
+            )}
+        </section>
+    );
+};
